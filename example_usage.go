ac030f44f48c1803f77700e566997f2f39af3889
Munir Ahmad <munir.ehmad@gmail.com>
2018-01-17 15:24:36 +0500

    Minor improvements, unexposed logger object

--- example_usage.go
+++ example_usage.go
@@ -6,7 +6,7 @@ import (
 	"github.com/munirehmad/golanglog/logger"
 )
 
-func main(){
+func main() {
 	log := logger.NewLogger("./log_test.log", true)
 	//log.Level = logger.LInfo
 	log.Debug("This is debug", 1, true, "nice debugging", log)
--- logger/logger.go
+++ logger/logger.go
@@ -1,10 +1,10 @@
 package logger
 
 import (
-	"os"
-	"time"
 	"fmt"
+	"os"
 	"strings"
+	"time"
 )
 
 const (
@@ -15,65 +15,69 @@ const (
 	LFatal
 )
 
-type Logger struct {
-	FileName string
-	File *os.File
-	Level uint
+type logger struct {
+	FileName      string
+	Level         uint
 	PrintToStdout bool
+
+	file *os.File
 }
 
-func NewLogger(file string, printToStdout bool) *Logger {
-	logger := Logger{FileName: file, PrintToStdout: printToStdout}
+func NewLogger(file string, printToStdout bool) *logger {
+	logger := logger{FileName: file, PrintToStdout: printToStdout}
 	var err error
 
 	if file != "" {
-		logger.File, err = os.OpenFile(file, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+		logger.file, err = os.OpenFile(file, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 		check(err)
 	}
 
 	return &logger
 }
 
-func (l Logger) Debug(args ...interface{}) {
+func (l *logger) Debug(args ...interface{}) {
 	if l.Level <= LDebug {
 		l.Log("DEBUG", args...)
 	}
 }
 
-func (l Logger) Info(args ...interface{}) {
+func (l *logger) Info(args ...interface{}) {
 	if l.Level <= LInfo {
 		l.Log("INFO", args...)
 	}
 }
 
-func (l Logger) Warn(args ...interface{}) {
+func (l *logger) Warn(args ...interface{}) {
 	if l.Level <= LInfo {
 		l.Log("WARN", args...)
 	}
 }
 
-func (l Logger) Error(args ...interface{}) {
+func (l *logger) Error(args ...interface{}) {
 	if l.Level <= LInfo {
 		l.Log("ERROR", args...)
 	}
 }
 
-func (l Logger) Fatal(args ...interface{}) {
+func (l *logger) Fatal(args ...interface{}) {
 	if l.Level <= LInfo {
 		panic(l.Log("FATAL", args...))
 	}
 }
 
-func (l Logger) Log(tag string, args ...interface{}) (msg string) {
+func (l *logger) Log(tag string, args ...interface{}) (msg string) {
 	// 2017/12/28 16:53:12
-	tfmt := "2006/01/2 15:04:05 MST"
-	prefix := time.Now().Format(tfmt)
-	msg = "" + tag + "|" + "" + prefix + "| " + fmtLogMsg(args...) + "\n"
+	timeFmt := "2006/01/2 15:04:05 MST"
+	timeStamp := time.Now().Format(timeFmt)
+	msg = "" + tag + "|" + "" + timeStamp + "| " + fmtLogMsg(args...) + "\n"
+
+	// log to stdout
 	fmt.Print(msg)
 
-	if l.File != nil {
-		l.File.WriteString(msg)
-		l.File.Sync()
+	// log to file if set
+	if l.file != nil {
+		l.file.WriteString(msg)
+		l.file.Sync()
 	}
 
 	return
@@ -94,7 +98,7 @@ func fmtLogMsg(args ...interface{}) string {
 	return strings.Join(argsStr, ", ")
 }
 
-func check(e error){
+func check(e error) {
 	if e != nil {
 		panic(e)
 	}
